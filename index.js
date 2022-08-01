dbb3d3deb7a55e7751101467157f793ed45f3107
Kachman21 <Kachman21onyedikachiokafor21@gmail.com>
2022-08-02 00:27:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+let array=[1,2,3]
+array[2]= "three"
+console.log(array)
+
+
+let tarray= ["anime","football","basketball","boxing","formula"]
+tarray[3]= "Wow, i did it"
+console.log(tarray)
+console.log(true&&false)
+console.log(true&&true)
+console.log(false&&false)
+if (14) {
+    console.log("your eligible for codeville,")
+ } else
+console.log("your not eligible for codeville,")
+ 
